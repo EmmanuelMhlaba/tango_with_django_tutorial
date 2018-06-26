9af362365fd0eeee97ae022fa38e97dc9b6e2385
Emmanuel Mhlaba <emmanuel.mhlaba@fnb.co.za>
2018-06-26 14:01:24 +0200

    Added rango app & updated gitignore.
    Added the rango app with a basic index page. Updated the
    gitignore file to ignore vscode files.

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf.urls import include
+
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    re_path(r'^rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
